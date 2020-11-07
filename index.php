ad960ad730eda5d985fb56321e148a299669c01c
AleixPrieto <aleix3d@gmail.com>
2020-11-07 18:37:11 +0100

    create index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title></title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="css/estils.css" />
+		<meta name="descriptions" content="">
+		<link rel="stylesheet" type="text/css" href="media/mobil.css" media="screen and (max-width: 500px)" />
+		<link rel="shortcut icon" href="imatges/favicon.ico" type="image/x-icon" />
+	</head>
+	<body>
+		<h1>Prueba</h1>
+	</body>
+</html>
